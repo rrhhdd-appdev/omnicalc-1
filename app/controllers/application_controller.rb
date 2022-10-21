7b0b1b18adf122e0c52ccccd1fb828ce60c0442b
rrhhdd <rharrisondavis@chicagobooth.edu>
2022-10-21 01:28:32 +0000

    Random Number (new and results) finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,16 @@ class ApplicationController < ActionController::Base
   end
 
 
+  def blank_rand_form
+    render ({ :template => "calculation_templates/rand_form.html.erb"})
+  end
+
+  def calculate_random
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+
+    render ({ :template => "calculation_templates/rand_results.html.erb"})
+  end
 
 end
